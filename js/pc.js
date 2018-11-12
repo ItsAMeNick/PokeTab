0f713364bc7fdc37295b3e783b88eb8e0f0cd943
ItsAMeNick <zonern@rpi.edu>
2018-11-12 15:51:12 -0500

    Added sort functionality to buttons

---
+++ js/pc.js
@@ -0,0 +1,40 @@
+document.getElementById("sort_egg").addEventListener("click", function() {
+	chrome.storage.local.get('eggPosition', function(result) {
+		if (!result.eggPosition) {
+			chrome.storage.local.set({'eggPosition': "top"});
+		} else if (result.eggPosition == 'top') {
+			chrome.storage.local.set({'eggPosition': "bottom"});
+		} else {
+			chrome.storage.local.set({'eggPosition': "top"});
+		}
+	});
+	location.reload();
+}, false);
+
+document.getElementById("sort_dex").addEventListener("click", function() {
+	chrome.storage.local.get('pcSort', function(result) {
+		if (!result.pcSort) {
+			chrome.storage.local.set({'pcSort': "DexL"});
+		} else if (result.pcSort == 'DexL') {
+			chrome.storage.local.set({'pcSort': "DexH"});
+		} else {
+			chrome.storage.local.set({'pcSort': "DexL"});
+		}
+	});
+	location.reload();
+}, false);
+
+document.getElementById("sort_level").addEventListener("click", function() {
+	chrome.storage.local.get('pcSort', function(result) {
+		if (!result.pcSort) {
+			chrome.storage.local.set({'pcSort': "LvL"});
+		} else if (result.pcSort == 'LvL') {
+			chrome.storage.local.set({'pcSort': "LvH"});
+		} else {
+			chrome.storage.local.set({'pcSort': "LvL"});
+		}
+	});
+	location.reload();
+}, false);
+
+//DexL
